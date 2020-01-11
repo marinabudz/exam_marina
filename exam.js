4b21ad9d7e281d212463d0e77827e6257f13ec1e
marina <budz.marina97@gmail.com>
2020-01-11 13:23:47 +0200

    new
    new

---
+++ exam.js
@@ -0,0 +1,28 @@
+$('.slider').slick(
+    {
+        dots: true,
+        arrows: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '60px',
+        fade: true,
+    }
+)
+$('.sliderClients').slick({
+
+    dots: false,
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '100px',
+
+})
+
